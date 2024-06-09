7586554aa493856085d2e5083ed4bd0a5cb3cf3d
wisnu andika <wisnuandika104@gmail.com>
2024-06-09 20:43:06 +0700

    feat: add navbar and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <div>
-        first setup
-      </div>
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import { Menu, X } from "lucide-react"
+import { useState } from 'react'
+
+export default function Navbar() {
+	const [mobileDrawerOpen, setMobileDrawer] = useState(false)
+
+	const toggleNavbar = () => {
+		setMobileDrawer(!mobileDrawerOpen)
+	}
+
+	const navItems = [
+		{ label: "Features", href: "#" },
+		{ label: "Workflow", href: "#" },
+		{ label: "Pricing", href: "#" },
+		{ label: "Testimobials", href: "#" },
+	]
+	return (
+		<nav className="sticky top-0 z-50 z-50 py-3 backdrop-blur-lg border border-neutral-700/80">
+			<div className="container px-4 mx-auto relative text-sm">
+				<div className="flex justify-between items-center">
+					<div className="flex items-center flex-shrink-0">
+						<img className='h-10 w-10 mr-2' src={logo} alt="" />
+						<span className="text-xl tracking-tight">VirtualIR</span>
+					</div>
+					<ul className="hidden lg:flex ml-14 space-x-12">
+						{navItems.map((item, index) => (
+							<li key={index}>
+								<a href={item.href}>{item.label}</a>
+							</li>
+						))}
+					</ul>
+					<div className="hidden lg:flex justify-center space-x-12 items-center">
+						<a href="#" className='py-2 px-3 border rounded-md'>Sign In</a>
+						<a href="#" className='py-2 px-3 bg-gradient-to-r from-orange-500 to-orange-800 rounded-md'>Create an account</a>
+					</div>
+					<div className="lg:hidden md:flex flex-col justify-end">
+						<button onClick={toggleNavbar}>
+							{mobileDrawerOpen ? <X /> : <Menu />}
+						</button>
+					</div>
+				</div>
+				{mobileDrawerOpen && (
+					<div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+						<ul>
+							{navItems.map((item, index) => (
+								<li key={index} className="py-4">
+									<a href={item.href}>{item.label}</a>
+								</li>
+							))}
+						</ul>
+						<div className="flex space-x-6 mt-2">
+							<a href="#" className="py-2 px-3 border rounded-md">Sign In</a>
+							<a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">Have an account</a>
+						</div>
+					</div>
+				)}
+			</div>
+		</nav>
+	)
+}
