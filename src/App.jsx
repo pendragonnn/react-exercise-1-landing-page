1c7aaa5e70ce266539b5c498b41bdd0f7374edb6
wisnu andika <wisnuandika104@gmail.com>
2024-06-10 10:32:07 +0700

    feat: add workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
+import WorkflowSection from './components/WorkflowSection'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <WorkflowSection />
       </div>
     </>
   )
